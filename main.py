951c3dd02b38a6d11ff16ee83970ef722b597fe3
nicholas <nicholaszingleman@gmail.com>
2024-03-29 06:21:33 -0400

    completed step 15

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ def main():
 	frankenstein_words = word_count(book_text)
 	print(f"There are {frankenstein_words} words in frankenstein.")
 
+	frankenstein_letters = letter_count(book_text)
+	print(frankenstein_letters)
 
 # Getting Text
 def get_book_text(path):
@@ -20,6 +22,24 @@ def word_count(text):
 	count = 0
 	for word in words:
 		count += 1
+	return count
+
+
+# Counting Letters
+def letter_count(text):
+	count = {}
+
+	text = text.lower()
+	words = text.split()
+	
+	for word in words:
+		for letter in word:
+
+			if letter not in count:
+				count[letter] = 1
+			else:
+				count[letter] += 1
+	
 	return count
 
 
