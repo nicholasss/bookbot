36dadd8bfff1a7bfc5c1be37a040edd1b81c92ff
nicholas <nicholaszingleman@gmail.com>
2024-03-29 06:07:02 -0400

    completed step 14

--- main.py
+++ main.py
@@ -1,1 +1,26 @@
-print("Hello, World!")
+def main():
+	book_path = "books/frankenstein.txt"
+	book_text = get_book_text(book_path)
+	print(book_text)
+
+	frankenstein_words = word_count(book_text)
+	print(f"There are {frankenstein_words} words in frankenstein.")
+
+
+# Getting Text
+def get_book_text(path):
+	with open(path) as f:
+		return f.read()
+
+
+# Counting Words
+def word_count(text):
+	words = text.split()
+
+	count = 0
+	for word in words:
+		count += 1
+	return count
+
+
+main()
